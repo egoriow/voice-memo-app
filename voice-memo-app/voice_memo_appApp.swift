68d822058b397cd16c9d7aa5266addc18062b285
Egor Podmarev <egoriow06@gmail.com>
2025-02-16 18:54:37 +0500

    Initial Commit

---
+++ voice-memo-app/voice_memo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  voice_memo_appApp.swift
+//  voice-memo-app
+//
+//  Created by Egor on 16/02/25.
+//
+
+import SwiftUI
+
+@main
+struct voice_memo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ voice-memo-appTests/voice_memo_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  voice_memo_appTests.swift
+//  voice-memo-appTests
+//
+//  Created by Egor on 16/02/25.
+//
+
+import Testing
+@testable import voice_memo_app
+
+struct voice_memo_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
